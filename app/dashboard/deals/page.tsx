c2f92aa1e26dcec0d153fb1f8b48934362c145b0
Evan Kiser <kiser.evan1@gmail.com>
2023-12-27 17:38:56 -0600

    Push a bunch of cards because I can't test these locally

--- app/dashboard/deals/page.tsx
+++ app/dashboard/deals/page.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import { DealCard } from "@/components/DealCard";
 
 export const dynamic = "force-dynamic";
@@ -7,12 +8,19 @@ export const dynamic = "force-dynamic";
 // See https://shipfa.st/docs/tutorials/private-page
 export default async function Deals() {
   return (
-    <main className="min-h-screen p-8 pb-24 flex items-center justify-center">
-        <section className="max-w-xl mx-auto space-y-8">
-            <div className="flex flex-col items-center">
-              <DealCard destination="New York" origin="London" points={1000} cabinClass="First" carrierLogo="https://1000logos.net/wp-content/uploads/2020/03/Emirates-Logo.png" carrier="Emirates" imageUrl="https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/03/25/12/eiffel.jpg" />
-            </div>
-        </section>
-    </main>
+    <>
+      <Header />
+      <main className="min-h-screen p-8 pb-24 flex items-center justify-center">
+          <section className="max-w-xl mx-auto space-y-8">
+              <div className="flex flex-col items-center">
+                <DealCard destination="New York" origin="London" points={1000} cabinClass="First" carrierLogo="https://1000logos.net/wp-content/uploads/2020/03/Emirates-Logo.png" carrier="Emirates" imageUrl="https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/03/25/12/eiffel.jpg" />
+                <DealCard destination="New York" origin="London" points={1000} cabinClass="First" carrierLogo="https://1000logos.net/wp-content/uploads/2020/03/Emirates-Logo.png" carrier="Emirates" imageUrl="https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/03/25/12/eiffel.jpg" />
+                <DealCard destination="New York" origin="London" points={1000} cabinClass="First" carrierLogo="https://1000logos.net/wp-content/uploads/2020/03/Emirates-Logo.png" carrier="Emirates" imageUrl="https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/03/25/12/eiffel.jpg" />
+                <DealCard destination="New York" origin="London" points={1000} cabinClass="First" carrierLogo="https://1000logos.net/wp-content/uploads/2020/03/Emirates-Logo.png" carrier="Emirates" imageUrl="https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/03/25/12/eiffel.jpg" />
+                <DealCard destination="New York" origin="London" points={1000} cabinClass="First" carrierLogo="https://1000logos.net/wp-content/uploads/2020/03/Emirates-Logo.png" carrier="Emirates" imageUrl="https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/03/25/12/eiffel.jpg" />
+              </div>
+          </section>
+      </main>
+    </>
   );
 }
